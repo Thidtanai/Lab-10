05d55e6d5c6d7e9ce39ee47a417c6725ac45af0d
Thidtanai <thidtanai_k@cmu.ac.th>
2021-01-12 16:06:30 +0700

    Lab10-1complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,36 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int randomgrade;
+
+void grade(){
+  randomgrade = rand()%9;
+  return;
+}
+
+string getgrade(int get){
+  if(get == 0)return "A";
+  if(get == 1)return "B+";
+  if(get == 2)return "B";
+  if(get == 3)return "C+";
+  if(get == 4)return "C";
+  if(get == 5)return "D+";
+  if(get == 6)return "D";
+  if(get == 7)return "F";
+  if(get == 8)return "W";
+  return "Error";
+}
+
+int main() {
+  srand(time(0));
+  grade();
+  cout << "Press Enter 3 times to reveal your future.";
+  cin.get();
+  cin.get();
+  cin.get();
+  cout << "You will get " << getgrade(randomgrade) << " in this 261102.";
+  return 0;
+}
